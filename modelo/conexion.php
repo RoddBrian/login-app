57347a4c0cc3e8354e2c0c6464f66cf2e1f3ca69
RoddBrian <roddbrian@outlook.es>
2024-05-06 15:31:32 -0500

    feat: :sparkles: Add code

---
+++ modelo/conexion.php
@@ -0,0 +1,21 @@
+<?php
+$user = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$server = getenv('DB_HOST');
+$database = getenv('DB_NAME');
+// $user = 'root';
+// $password = '4Doradas';
+// $server = '127.0.0.1';
+// $database = 'APP';
+
+$conexion = new mysqli($server, $user, $password, $database);
+
+if ($conexion->connect_errno) {
+    printf("Connect failed: %s\n", $conexion->connect_error);
+    exit();
+}else{
+    printf("Connect success");
+}
+
+$conexion->set_charset("utf8");
+?>
